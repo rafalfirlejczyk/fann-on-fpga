0731e3b8f049001c93bd055c6f6e06eca4df5fc4
laliloulelo <laliloulelo@aad92eba-1fc9-11df-902a-156ec2e33ad3>
2010-03-24 21:31:46 +0000

    first commit - installation

---
+++ src/NeuralNetwork.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : NeuralNetwork.cpp
+// Author      : Guillaume VINCENT http://vincentguillaume.fr/
+// Version     :
+// Copyright   : no copyright
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
